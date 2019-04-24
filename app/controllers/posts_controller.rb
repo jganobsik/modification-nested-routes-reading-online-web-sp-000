3841965d7c62fc29f84f746249b50cb058ff4124
Joe Ganobsik <jganobsik@gmail.com>
2019-04-24 14:27:59 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ end
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
+def post_params
+  params.require(:post).permit(:title, :description, :author_id)
+end
 end
